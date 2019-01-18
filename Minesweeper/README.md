910e32c79133786ae5f74fb2c55ce9f55c6f89e4
Allison Walke <awalke796@gmail.com>
2019-01-17 21:36:56 -0800

    redesign

--- Minesweeper/README.md
+++
@@ -1,3 +0,0 @@
-# Minesweeper
-
-![alt tag](https://raw.githubusercontent.com/awalke/Minesweeper/master/Media/minesweeper.png)
--- app.js
+++ app.js
@@ -1,81 +1,11 @@
-$(document).ready(function (){
-  $("#name").hide();
-  $(".menu").hide();
-  $(".description").hide();
-  $(".projects").hide();
-
-  $("#about").click(function(){
-    aboutMeClick();
+$(document).ready(function() {
+  $(".container-front").click(function () {
+    $('.content').css('transition', '1s');
+    $('.content').css('transform', 'rotateY( -180deg )');
   });
 
-  $("#projects").click(function(){
-    projectsClick();
+  $(".back").click(function () {
+    $('.content').css('transition', '1s');
+    $('.content').css('transform', 'rotateY( 0deg )');
   });
-
-  $("#exit").click(function () {
-    exitClick();
-  });
-
-  setTimeout(function() {
-    $("#name").fadeIn(1000);
-    showAbout();
-  }, 500);
 });
-
-function projectsClick() {
-  $(".menu").fadeOut(500);
-  $(".projects").fadeIn(1000);
-  $(".projects").css("z-index", "2");
-  $(".menu").css("z-index", "1");
-}
-
-function exitClick() {
-  $(".menu").fadeIn(1000);
-  $(".projects").fadeOut(500);
-  $(".menu").css("z-index", "2");
-  $(".projects").css("z-index", "1");
-}
-
-
-function aboutMeClick() {
-  // $(".landingPage").css("background-image", "url(media/koda-fine-bg.png)");
-  if (window.matchMedia('screen and (min-width:400px)').matches && !window.matchMedia('screen and (min-width:1000px)').matches) {
-    $(".landingPage").css("background-image", "url(media/canvas.png)");
-  }
-  $(".description").fadeIn(1000);
-
-}
-
-function showAbout() {
-  setTimeout(function() {
-    $("#about").fadeIn(1000);
-    showResume();
-  }, 1000);
-}
-
-function showResume() {
-  setTimeout(function() {
-    $("#resume").fadeIn(1000);
-    showProjects();
-  }, 300);
-}
-
-function showProjects() {
-  setTimeout(function() {
-    $("#projects").fadeIn(1000);
-    showGitHub();
-  }, 300);
-}
-
-function showGitHub() {
-  setTimeout(function() {
-    $("#github").fadeIn(1000);
-    showLinkedIn();
-  }, 300);
-}
-
-function showLinkedIn() {
-  setTimeout(function() {
-    $("#linkedin").fadeIn(1000);
-  }, 300);
-}
