95c9b022890c9efe452df0404512a3aa02294118
Allison Walke <awalke796@gmail.com>
2019-01-23 20:14:46 -0800

    back left side flip on click now working

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 $(document).ready(function() {
-  $(".container-front").click(function () {
-    $('.front-center').hide();
-    $('.square').hide();
-    $('.content').css('-webkit-transition', '1s');
-    $('.content').css('-webkit-transform', 'rotateY( -180deg )');
+  var state = 0;
 
+  $(".container-front").click(function () {
+
+    if (state == 1) {
+      state = 0;
+      $('.front-center').show();
+      $('.square').show();
+      $('.content').css('-webkit-transition', '1s');
+      $('.content').css('-webkit-transform', 'rotateY( 0deg )');
+    }
+    else {
+      state = 1;
+      $('.front-center').hide();
+      $('.square').hide();
+      $('.content').css('-webkit-transition', '1s');
+      $('.content').css('-webkit-transform', 'rotateY( -180deg )');
+    }
   });
 
   $(".back").click(function () {
+    state = 0;
     $('.front-center').show();
     $('.square').show();
     $('.content').css('-webkit-transition', '1s');
