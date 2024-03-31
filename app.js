155c43dd3b4eba924539023e9c1024c5558defda
Allison Walke <allisonwalke@Allisons-MBP.home>
2024-03-31 00:04:47 -0600

    animate clouds on landing page

--- app.js
+++ app.js
@@ -26,6 +26,31 @@ document.addEventListener('DOMContentLoaded', function() {
     addClickToScrollListener(document.getElementById('aboutItem'), document.querySelector('.aboutMeSection'));
     addClickToScrollListener(document.getElementById('experienceItem'), document.querySelector('.resumeContainer'));
     addClickToScrollListener(document.getElementById('contactItem'), document.querySelector('.contactContainer'));
+
+    const cloud1 = document.querySelector('#cloud1');
+
+    // When animation ends, reposition the element
+    cloud1.addEventListener('animationiteration', () => {
+        // Reset element position to start from the left side
+        cloud1.style.setProperty('--cloud-1-start', '-150px');
+    });
+
+    const cloud2 = document.querySelector('#cloud2');
+
+    // When animation ends, reposition the element
+    cloud2.addEventListener('animationiteration', () => {
+        // Reset element position to start from the left side
+        cloud2.style.setProperty('--cloud-2-start', '-80px');
+    });
+
+    const cloud3 = document.querySelector('#cloud3');
+
+    // When animation ends, reposition the element
+    cloud3.addEventListener('animationiteration', () => {
+        const viewWidthPlus100px = window.innerWidth + 100;
+        // Reset element position to start from the left side
+        cloud3.style.setProperty('--cloud-3-start', viewWidthPlus100px.toString() + 'px');
+    });
 });
 
 function addClickToScrollListener(element, targetScroll) {
